d2ca580722edbc6254eed0fcf77944c07dcdb4ed
orblazer <orblazer.minecraft@gmail.com>
2020-07-20 18:18:37 +0200

    Initial commit

---
+++ src/main/java/fr/orblazer/bungeeRcon/Main.java
@@ -0,0 +1,11 @@
+package fr.orblazer.bungeeRcon;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * The main class of plugin
+ *
+ * @author orblazer
+ */
+public class Main extends Plugin {
+}
